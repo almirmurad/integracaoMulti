dd30d9a77da1b85ce00aa0bf1f086d0dd0bf8cee
almirmurad <developer@webmurad.com.br>
2025-08-27 19:33:55 -0300

    fix stock line data table stock file

---
+++ src/views/pages/gerenciador.pages.dataStockTable.php
@@ -0,0 +1,7 @@
+<tr>
+    <td class="statusDeEstoque">🟢 Ativo</td>
+    <td class="localDeEstoque">{local}</td>
+    <td>{saldo}</td>
+    <td>{minimo}</td>
+    <td class="previsaoDeSaida">{previsaoSaida}</td>
+</tr>
